149f91fadf3bd284151d9b308e670ceb783c2f40
kahitoz <kshitiz.sinha.025@gmail.com>
2023-07-30 01:29:54 +0530

    File Formatting Dashboard Routes Changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import Dashboard from "./Components/Dashboard";
+import Home from "./Components/D0_DashboardComponent/D0_Screens/D0_Home";
 
 function App() {
   return (
     <div className="App">
    <Router>
     <Routes>
-      <Route path = "*" element={<Dashboard/>}/>
+      <Route path = "*" element={<Home/>}/>
     </Routes>
    </Router>
     </div>
