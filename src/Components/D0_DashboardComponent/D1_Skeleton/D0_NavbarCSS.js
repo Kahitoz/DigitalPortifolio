a9b3dbdce7e47acde611aefbd3968c81ba937736
kahitoz <kshitiz.sinha.025@gmail.com>
2023-07-30 00:01:35 +0530

    CSS File for the navbarCSS

---
+++ src/Components/D0_DashboardComponent/D1_Skeleton/D0_NavbarCSS.js
@@ -0,0 +1,9 @@
+export const D0_NavbarCSS = {
+    d1: "shadow-md w-full fixed top-0 left-0",
+    d2: "md:flex items-center justify-between px-4 bg-black py-4 text-white font-bold",
+    d3: "font-bold text-white px-7 text-xl cursor-pointer flex items-center  hover:text-gray-400 duration-200",
+    d4: "text-3xl absolute right-8 top-3 cursor-pointer md:hidden",
+    d5: "md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-black md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-11 transition-all duration-500 ease-in",
+    d6: "md:ml-8 md:my-0 my-7",
+    d7: "text-white hover:text-gray-400 duration-200",
+};
