13ab07a6e011b15cdb0f97c67842ded52a173a16
kahitoz <kshitiz.sinha.025@gmail.com>
2023-07-30 01:31:22 +0530

    TitleHolder Screen

---
+++ src/Components/D0_DashboardComponent/D1_Skeleton/D2_TitleHolder.js
@@ -0,0 +1,16 @@
+const D2_TitleHolder = () =>{
+    return(
+        <>
+            <div>
+                <div className="text-7xl mt-1 sm:mt-20 py-10">
+                    <h1>Hi there,</h1>
+                    <h1>I'm</h1>
+                    <p className="text-gradient bg-clip-text text-transparent bg-gradient-to-r from-blue-500 to-purple-500">Kshitiz Sinha</p>
+                </div>
+            </div>
+        </>
+
+    )
+}
+
+export default D2_TitleHolder;
