d1848c29ea3c3e57e4fa67f2f00948d01c8e610c
kahitoz <kshitiz.sinha.025@gmail.com>
2023-08-10 23:21:56 +0530

    hover css changed

--- src/Components/D0_DashboardComponent/D1_Skeleton/D4_SkillHolder.js
+++ src/Components/D0_DashboardComponent/D1_Skeleton/D4_SkillHolder.js
@@ -1,7 +1,7 @@
 const D4_SkillHolder = ({image,title, value}) =>{
     return(
         <>
-            <div className={`bg-gray-800 w-24 h-32 rounded-md hover:cursor-pointer shadow-md shadow-cyan-400 flex flex-col justify-center items-center sm:w-48 sm:h-64`}>
+            <div className={`bg-gray-800 w-24 h-32 rounded-md hover:cursor-pointer shadow-md hover:shadow-2xl hover:shadow-cyan-400 shadow-cyan-400 flex flex-col justify-center items-center sm:w-48 sm:h-64`}>
                 <img src={image} alt={title} className={`mx-auto w-4 h- sm:w-auto sm:h-auto`} />
                 <p className={`text-white font-bold text-center text-xs`}>{title}</p>
                 <div className="w-full mt-4 flex justify-evenly">
