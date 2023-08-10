e64c5b9e0c330351fe32bc85d4773b3371856525
kahitoz <kshitiz.sinha.025@gmail.com>
2023-08-10 22:43:11 +0530

    Responsive CSS

--- src/Components/D0_DashboardComponent/D1_Skeleton/D1_PhotoHolder.js
+++ src/Components/D0_DashboardComponent/D1_Skeleton/D1_PhotoHolder.js
@@ -6,9 +6,8 @@ const D1_PhotoHolder = () =>{
                 <img
                     src={myPhoto}
                     alt="Kshitiz Sinha Profile Photo"
-                    className={`rounded-full border-black border-2 hover:cursor-pointer hover:border-pink-950 w-64 h-64 transition-transform transform hover:scale-105`}
+                    className={`rounded-full border-black border-2 w-32 h-32 hover:cursor-pointer hover:border-pink-950 sm:w-64 sm:h-64 transition-transform transform hover:scale-105`}
                 />
-
             </div>
 
         </>
