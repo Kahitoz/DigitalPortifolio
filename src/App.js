6691dfd109d40ac828788b62a2b3f612e920754c
kahitoz <kshitiz.sinha.025@gmail.com>
2023-08-11 17:43:35 +0530

    Added Route for Projects Section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from "./Components/D0_DashboardComponent/D0_Screens/D0_Home";
+import Projects from "./Components/D1_ProjectComponent/D1_Screens/D0_Projects";
 
 function App() {
   return (
     <div className="App">
    <Router>
     <Routes>
-      <Route path = "*" element={<Home/>}/>
+        <Route path = "*" element={<Home/>}/>
+        <Route path = "/Projects" element={<Projects/>}/>
     </Routes>
    </Router>
     </div>
