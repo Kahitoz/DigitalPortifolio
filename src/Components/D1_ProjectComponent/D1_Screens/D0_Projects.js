0514f4e4e9459475a78ec420ec281cebcbd9cfef
kahitoz <kshitiz.sinha.025@gmail.com>
2023-08-11 19:23:08 +0530

    Changed the path For the project section

--- src/Components/D1_ProjectComponent/D1_Screens/D0_Projects.js
+++ src/Components/D1_ProjectComponent/D1_Screens/D0_Projects.js
@@ -1,10 +1,17 @@
 import Navbar from "../../../Assets/Navbar";
+import Projects from "../D2_Skeleton/D1_ProjectSection";
 const D0_Projects = () =>{
     return(
         <>
-            <div className={`h-screen w-screen bg-black`}>
-                <Navbar/>
+            <div className={`w-screen h-screen bg-black`}>
+                <div>
+                    <Navbar/>
+                </div>
+                <div className={`py-20`}>
+                    <Projects/>
+                </div>
             </div>
+
         </>
     )
 }
