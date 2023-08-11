24018ef3dd2e547b8a29d1e0255ea28adcebbf06
kahitoz <kshitiz.sinha.025@gmail.com>
2023-08-11 19:23:30 +0530

    Completed the project fetching functionality

---
+++ src/Components/D1_ProjectComponent/D2_Skeleton/D1_ProjectSection.js
@@ -0,0 +1,54 @@
+import D0_Projects from "../D1_Screens/D0_Projects";
+import {db} from "../../../Database/firebase";
+import {useEffect, useState} from "react";
+import {collection, getDocs} from "firebase/firestore"
+import github from "../../D0_DashboardComponent/D2_Images/github.png";
+
+const D1_ProjectSection = ()=>{
+    const [projects, setProjects] = useState([]) ;
+    const collecRef = collection(db, "Projects")
+    useEffect(()=>{
+        const fetchData = async () => {
+            try {
+                const data  = await getDocs(collecRef)
+                setProjects(data.docs.map((doc)=>({...doc.data(), id:doc.id})));
+                console.log(data);
+            } catch (error) {
+                console.error("Firestore Error = ",error);
+            }
+        }
+        fetchData();
+
+    },[])
+    return(
+        <>
+            <div className={`bg-black py-1 px-4 sm:px-28 sm:py-2`}>
+                <p className={`font-bold text-white mt-1 mb-4 text-2xl text-center sm:mt-2`}>Programs/Projects</p>
+            <div>
+                {projects.map((project)=>{
+                    return  <div>
+                        <div className={`bg-gray-800 rounded-xl p-4 hover:cursor-pointer hover:shadow-2xl hover:shadow-cyan-400 hover:rounded-2xl hover:bg-gray-600 shadow-md shadow-cyan-400`}>
+                            <div className={`flex justify-between`}>
+                                <p className={`text-white font-bold text-2xl`}>{project.title}</p>
+                                <img src={project.img0}
+                                     className={`w-16 h-16 rounded-full`} alt={project.title}/>
+                            </div>
+                            <div>
+                                <p className={`text-xl font-bold text-white text-center`}>{project.short_desc}</p>
+                                <p className={`text-md font-bold text-white text-center mt-4`}>Skills: {project.skills}</p>
+                            </div>
+                            <div className={`mt-5 flex justify-between`}>
+                                <p className={`text-sm font-bold text-white text-center`}>From {project.start}</p>
+                                <p className={`text-sm font-bold text-white text-center`}>Status: {project.status}</p>
+                                <p className={`text-sm font-bold text-white text-center`}>To:{project.end}</p>
+                            </div>
+                        </div>
+                    </div>
+
+                })}
+            </div>
+            </div>
+        </>
+    )
+}
+export default D1_ProjectSection;
