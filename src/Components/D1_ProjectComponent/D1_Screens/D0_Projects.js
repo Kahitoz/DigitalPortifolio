4b8dee370f717b8545c4b49379cddaab3ee3243c
kahitoz <kshitiz.sinha.025@gmail.com>
2023-08-11 17:44:06 +0530

    New Project Screen

---
+++ src/Components/D1_ProjectComponent/D1_Screens/D0_Projects.js
@@ -0,0 +1,11 @@
+import Navbar from "../../../Assets/Navbar";
+const D0_Projects = () =>{
+    return(
+        <>
+            <div className={`h-screen w-screen bg-black`}>
+                <Navbar/>
+            </div>
+        </>
+    )
+}
+export default D0_Projects;
