978b9c31f8d3c5419439b204d6307cc5f5b0b67d
kahitoz <kshitiz.sinha.025@gmail.com>
2023-08-13 21:28:24 +0530

    Some CSS changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Home from "./Components/D0_DashboardComponent/D0_Screens/D0_Home";
 import Projects from "./Components/D1_ProjectComponent/D1_Screens/D0_Projects";
+import ProjectInfo from "./Components/D1_ProjectComponent/D2_Skeleton/D2_ProjectInfo";
 
 function App() {
   return (
     <div className="App">
-   <Router>
-    <Routes>
-        <Route path = "*" element={<Home/>}/>
-        <Route path = "/Projects" element={<Projects/>}/>
-    </Routes>
-   </Router>
+        <div className={`h-screen bg-black`}>
+            <Router>
+                <Routes>
+                    <Route path = "*" element={<Home/>}/>
+                    <Route path = "/Projects" element={<Projects/>}/>
+                    <Route path = "/Projects/Info" element={<ProjectInfo/>}/>
+                </Routes>
+            </Router>
+        </div>
     </div>
   );
 }
