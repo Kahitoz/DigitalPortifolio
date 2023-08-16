2f6b2d601cd00032fce50ee8d1fb09695498f1c3
Kahitoz <kshitiz.sinha.025@gmail.com>
2023-08-17 00:57:50 +0530

    Added the props for highlighting

--- src/Assets/Navbar.js
+++ src/Assets/Navbar.js
@@ -2,46 +2,52 @@ import React, { useState } from "react";
 import Button from "./Button";
 import { D0_NavbarCSS } from "../Components/D0_DashboardComponent/D1_Skeleton/D0_NavbarCSS";
 
-const Navbar = () => {
-    let Links = [
-        { name: "Home", link: "/" },
-        { name: "Résumé", link: "/" },
-        { name: "Projects", link: "/Projects" },
-        { name: "Contact Me", link: "/" },
-    ];
+const Navbar = ({ tab }) => {
+  let Links = [
+    { name: "Home", link: "/" },
+    { name: "Resume", link: "/Resume" },
+    { name: "Projects", link: "/Projects" },
+    { name: "Contact Me", link: "/" },
+  ];
 
-    let [open, setOpen] = useState(false);
-    return (
-        <body className="bg-black">
-        <div>
-            <div className={`${D0_NavbarCSS.d1}`}>
-                <div className={`${D0_NavbarCSS.d2}`}>
-                    <div className={`${D0_NavbarCSS.d3}`}>Welcome</div>
-                    <div
-                        onClick={() => setOpen(!open)}
-                        className={`${D0_NavbarCSS.d4}`}
-                    >
-                        <ion-icon name="grid-outline"></ion-icon>
-                    </div>
-                    <ul
-                        className={`${D0_NavbarCSS.d5} ${
-                            open ? "top-10" : "top-[-490px]"
-                        }`}
-                    >
-                        {Links.map((link) => (
-                            <li key={link.name} className={`${D0_NavbarCSS.d6}`}>
-                                <a href={link.link} className={`${D0_NavbarCSS.d7}`}>
-                                    {link.name}
-                                </a>
-                            </li>
-                        ))}
-                        <Button>Switch</Button>
-                    </ul>
-                </div>
+  let [open, setOpen] = useState(false);
+
+  return (
+    <body className="bg-black">
+      <div>
+        <div className={`${D0_NavbarCSS.d1}`}>
+          <div className={`${D0_NavbarCSS.d2}`}>
+            <div className={`${D0_NavbarCSS.d3}`}>Welcome</div>
+            <div
+              onClick={() => setOpen(!open)}
+              className={`${D0_NavbarCSS.d4}`}
+            >
+              <ion-icon name="grid-outline"></ion-icon>
             </div>
+            <ul
+              className={`${D0_NavbarCSS.d5} ${
+                open ? "top-10" : "top-[-490px]"
+              }`}
+            >
+              {Links.map((link) => (
+                <li key={link.name} className={`${D0_NavbarCSS.d6}`}>
+                  <a
+                    href={link.link}
+                    className={`${D0_NavbarCSS.d7} ${
+                      link.name === tab ? "text-cyan-300" : ""
+                    }`}
+                  >
+                    {link.name}
+                  </a>
+                </li>
+              ))}
+              <Button>Switch</Button>
+            </ul>
+          </div>
         </div>
-        </body>
-    );
+      </div>
+    </body>
+  );
 };
 
 export default Navbar;
