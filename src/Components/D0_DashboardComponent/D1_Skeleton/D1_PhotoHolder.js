2cc7feecad13a7d2fc86071019750f1643dd1eb4
Kahitoz <kshitiz.sinha.025@gmail.com>
2023-08-18 23:24:57 +0530

    Prettier File Formatting

--- src/Components/D0_DashboardComponent/D1_Skeleton/D1_PhotoHolder.js
+++ src/Components/D0_DashboardComponent/D1_Skeleton/D1_PhotoHolder.js
@@ -1,16 +1,15 @@
-import myPhoto from "../D2_Images/ProfilePhoto.png"
-const D1_PhotoHolder = () =>{
-    return(
-        <>
-            <div >
-                <img
-                    src={myPhoto}
-                    alt="Kshitiz Sinha Profile Photo"
-                    className={`rounded-full border-black border-2 w-32 h-32 hover:cursor-pointer hover:border-pink-950 sm:w-64 sm:h-64 transition-transform transform hover:scale-105`}
-                />
-            </div>
-
-        </>
-    );
-}
+import myPhoto from "../D2_Images/ProfilePhoto.png";
+const D1_PhotoHolder = () => {
+  return (
+    <>
+      <div>
+        <img
+          src={myPhoto}
+          alt="Kshitiz Sinha Profile Photo"
+          className={`rounded-full border-black border-2 w-32 h-32 hover:cursor-pointer hover:border-pink-950 sm:w-64 sm:h-64 transition-transform transform hover:scale-105`}
+        />
+      </div>
+    </>
+  );
+};
 export default D1_PhotoHolder;
