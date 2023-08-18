621fb70298df27af2bd22bfd78718870f6bc375a
Kahitoz <kshitiz.sinha.025@gmail.com>
2023-08-18 23:24:44 +0530

    Prettier File Formatting

--- src/Assets/Button.js
+++ src/Assets/Button.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-const Button = (props) =>{
-    return(
-        <button className="bg-indigo-800 text-white py-2 px-6 rounded md:ml-8 hover:bg-indigo-300 duration-500">
-           {props.children}
-        </button>
-    );
-}
+const Button = (props) => {
+  return (
+    <button className="bg-indigo-800 text-white py-2 px-6 rounded md:ml-8 hover:bg-indigo-300 duration-500">
+      {props.children}
+    </button>
+  );
+};
 export default Button;
